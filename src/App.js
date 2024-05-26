5d08493a244b70f6d7bdc143d30555641dd84f66
Nehakumari123123 <techieneha.it@gmail.com>
2024-05-26 18:35:06 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  let [a, seta] = useState(0);
+  const handleincrement = () => {
+    seta(a + 1);
+    console.log(a);
+  };
+
+  const handledecrement = () => {
+    seta(a - 1);
+    console.log(a);
+  };
+
+  return (
+    <div>
+      <button className="increment" onClick={handleincrement}>
+        +
+      </button>
+
+      <textarea name="" id="" cols="30" rows="10" readOnly value={a}>
+        
+        {a}
+      </textarea>
+
+      <button className="decrement" onClick={handledecrement}>
+        -
+      </button>
+    </div>
+  );
+}
+
+export default App;
